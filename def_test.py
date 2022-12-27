45f89869a0318eeb74cd1d754218da8d81b3c112
Gongback <94734506+newkincode@users.noreply.github.com>
2022-12-28 00:56:59 +0900

    1.0

---
+++ def_test.py
@@ -0,0 +1,35 @@
+import pibo
+pibo(100)
+'''
+def plu(c, d):
+    return c + d
+def min(c, d):
+    return c - d
+def mul(c, d):
+    return c * d
+def div(c, d):
+    return c / d
+def rem(c, d):
+    return c % d
+
+print(mul(1,2))
+print(div(1,2))
+print(min(1, 2))
+print(plu(1, 2))
+print(rem(1, 2))
+'''
+
+'''
+d = 0
+
+def gugudan(c):
+    a = 1
+    for b in range(0,9):
+        print("%d*%d=%d"%(c, a, c*a))
+        a += 1
+    a = 1
+
+for j in range(2, 10
+               0):
+    gugudan(j)
+'''
