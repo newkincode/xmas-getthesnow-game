2682ab79d06a8762595cf0dfcd673f0625e36680
DESKTOP-H9OD0J1\gongb <maainnewkin59@gmail.com>
2022-12-30 18:06:14 +0900

    커밋할 내용

---
+++ pibo.py
@@ -0,0 +1,11 @@
+
+def pibo(a):
+    pibo = [0, 1]
+    while True:
+        if pibo[-1] > a:
+            break
+        b = pibo[-2] + pibo[-1]
+        pibo.append(b)
+    pibo.pop(-1)
+    return pibo
+print(pibo())
