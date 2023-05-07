8aeafb7fd52d1065ddb92d2ed68b06b5d1ca013e
newkincode <maainnewkin59@gmail.com>
2023-05-07 20:13:30 +0900

    혹시몰라 저작권 걸릴지

--- pibo.py
+++ pibo.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 def pibo(a):
     pibo = [0, 1]
     while True:
@@ -9,16 +7,4 @@ def pibo(a):
         pibo.append(b)
     pibo.pop(-1)
     return pibo
-=======
-
-def pibo(a):
-    pibo = [0, 1]
-    while True:
-        if pibo[-1] > a:
-            break
-        b = pibo[-2] + pibo[-1]
-        pibo.append(b)
-    pibo.pop(-1)
-    return pibo
->>>>>>> 45f89869a0318eeb74cd1d754218da8d81b3c112
 print(pibo())
